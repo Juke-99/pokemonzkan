8cc6acaefed466c020165782db73185704987bac
Juke-99 <yanpahika@gmail.com>
2020-02-22 09:51:46 +0900

    add gorst species

--- src/Species.js
+++ src/Species.js
@@ -4,6 +4,7 @@ import SpeciesContent from './SpeciesContent.js';
 import leafSpecies from './json/leafSpecies';
 import waterSpecies from './json/waterSpecies';
 import fireSpecies from './json/fireSpecies';
+import gorstSpecies from './json/gorstSpecies';
 
 class Species extends Component {
   render() {
@@ -12,6 +13,7 @@ class Species extends Component {
         <SpeciesContent species={leafSpecies}></SpeciesContent>
         <SpeciesContent species={waterSpecies}></SpeciesContent>
         <SpeciesContent species={fireSpecies}></SpeciesContent>
+        <SpeciesContent species={gorstSpecies}></SpeciesContent>
       </div>
     );
   }
